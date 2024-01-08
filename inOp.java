1a55c5334e71fcfcfc5f051089278d1f3c2f04e4
yatjain17 <yatjain@deloitte.com>
2024-01-08 12:21:02 +0530

    first commit

---
+++ inOp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class inop {
+   
+    public static void main(String args []) {
+        System.out.print("hello to my New Program \n");
+        final Scanner s= new Scanner(System.in);
+        System.out.print("Please tell me your name: ");
+        String name=s.next();
+        System.out.println("\n\n Hello "+name+"!!!!");
+        
+    }
+}
